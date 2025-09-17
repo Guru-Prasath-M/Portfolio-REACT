7fc1814621a250f93f508a898d728def2e4e8bcb
Guru-Prasath-M <guruprasath908018@gmail.com>
2025-09-17 17:32:05 +0530

    Skills

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
- const Skills= () => {
+const Skills = () => {
   return (
-    <div>
-      <h2>Skills Components</h2>
+    <div className='container mt-3'>
+        <h2 className='text-center text-primary' style={{fontWeight:'bold'}}>My Skills</h2>
+      <div className='mb-3'>
+        <h4>HTML / CSS</h4>
+        <div className='progress'>
+            <div className="progress-bar bg-success" role='progressbar' style={{width:'85%'}}> 85%</div>
+        </div>
+      </div>
+
+      <div className='mb-3'>
+        <h4>JavaScript</h4>
+        <div className="progress">
+            <div className="progress-bar bg-info" role='progressbar' style={{width:'60%'}}>60%</div>
+        </div>
+      </div>
     </div>
   )
 }
+
 export default Skills;
