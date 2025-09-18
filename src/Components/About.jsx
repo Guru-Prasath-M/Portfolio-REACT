25c82dbffc63638700cfadc517b375c783a60e83
Guru-Prasath-M <guruprasath908018@gmail.com>
2025-09-18 20:00:42 +0530

    Skills & Contact Page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -37,7 +37,7 @@ const About = () => {
           {/* Text */}
           <div className="col-md-6">
             <h1 className="fw-bold mb-3 text-primary">I'm GuruPrasath</h1>
-            <p style={{ fontSize: "18px", lineHeight: "1.8" }}>
+            <p style={{ fontSize: "22px", lineHeight: "1.8" }}>
               Hi, I’m <b>GuruPrasath</b>, a passionate Full Stack Developer in the making.
               I recently graduated and I’m currently pursuing a <b>Java Full Stack Development</b> course.
               Skilled in <b>React.js, Bootstrap, SQL, and MongoDB</b>, I enjoy creating clean and responsive web applications.
